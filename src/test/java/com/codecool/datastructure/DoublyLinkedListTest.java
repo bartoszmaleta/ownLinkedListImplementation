df6e391b950359538e0fed6bc77b94d069848868
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-03 17:40:12 +0200

    Add test class with empty methods

---
+++ src/test/java/com/codecool/datastructure/DoublyLinkedListTest.java
@@ -0,0 +1,28 @@
+package com.codecool.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoublyLinkedListTest {
+
+    @Test
+    void access() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void addAtTail() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
