7bd847f9cde4b8d3b357c7ce61b5de5f34e5fc01
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-03 16:00:52 +0200

    Add implementation of LinkedList methods

--- src/main/java/com/codecool/datastructure/SinglyLinkedList.java
+++ src/main/java/com/codecool/datastructure/SinglyLinkedList.java
@@ -6,43 +6,128 @@ public class SinglyLinkedList {
 
         private int value;
         private Link next;
+        private int index;
 
         Link(int value) {
             this.value = value;
         }
 
-        int getValue() {
+        public Link setValue(int value) {
+            this.value = value;
+            return this;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public Link setIndex(int index) {
+            this.index = index;
+            return this;
+        }
+
+        public int getValue() {
             return value;
         }
 
-        Link getNext() {
+        public Link getNext() {
             return next;
         }
 
-        void setNext(Link next) {
+        public Link setNext(Link next) {
             this.next = next;
+            return this;
         }
     }
 
     private Link head;
+    private Link tail;
+    private int counter;
 
     public SinglyLinkedList() {
     }
 
     // Returns the number at 'index'.
     public int access(int index) {
-        return 0;
+        if (index == 0) return this.head.getValue();
+        if (head == null) return -1;
+        if (index > counter) {
+            throw new IndexOutOfBoundsException("Too big index");
+        }
+
+        Link current = this.head;
+        int currentIndex = 0;
+
+        while (currentIndex != index) {
+            currentIndex++;
+            current = current.getNext();
+        }
+        return current.getValue();
     }
 
     // Returns the index of 'number' if it's in the array, otherwise -1;
     public int search(int number) {
-        return 0;
+        if (number == 0) return 0;
+        if (head == null) return -1;
+
+        Link current = this.head;
+        int currentIndex = 0;
+        while (current != null) {
+            if (current.getValue() == number) {
+                return currentIndex;
+            }
+            currentIndex++;
+            current = current.getNext();
+        }
+        return -1;
     }
 
     // Inserts 'number' at 'index' into the array shifting elements if necessary.
     //
     // e.g. the result of inserting 42 at index 3 into [0, 1, 2, 3, 4] is [0, 1, 2, 42, 3, 4]
     public void insert(int index, int number) {
+        Link newNode = new Link(number);
+//        if (index > counter) {
+//            throw new IndexOutOfBoundsException("Too big number");
+//        }
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+            return;
+        }
+        if (index == 0) {
+            newNode.setNext(this.head);
+            this.head = newNode;
+        } else if (index == counter) {
+            this.tail.setNext(newNode);
+            tail = newNode;
+        } else {
+            Link current = this.head;
+
+            for (int i = 0; i < index; i++) {
+                current = current.getNext();
+            }
+
+            Link previous = this.head;
+            for (int i = 0; i < index - 1; i++) {
+                previous = previous.getNext();
+            }
+            newNode.setNext(current);
+            previous.setNext(newNode);
+        }
+        counter++;
+    }
+
+    public void addAtTail(int number) {
+        Link newNode = new Link(number);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+            return;
+        }
+        this.tail.setNext(newNode);
+        newNode.setIndex(this.tail.getIndex() + 1);
     }
 
     // Deletes the element at 'index' from the array.
